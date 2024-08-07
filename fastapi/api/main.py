bba3899a059e7f9f31fb0c5c3774d1035f8d91df
assanalibatyrkhan <asankek12@gmail.com>
2024-08-07 16:45:54 +0500

    authorization using bearer, user creation added

---
+++ fastapi/api/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.routers import auth
+
+from api.database import Base, engine
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+app.include_router(router=auth.router)
